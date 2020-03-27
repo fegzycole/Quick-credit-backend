15502c93e963e71ed2e20d40ce8e02fb56c7536b
fegzycole <fergusoniyara@gmail.com>
2020-03-27 21:35:11 +0100

    Setup user model validation and add unique constraint to user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :firstName, presence: true
+  validates :lastName, presence: true
+  validates :address, presence: true
+  validates :isAdmin, presence: true
+  validates :password,
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
 end
